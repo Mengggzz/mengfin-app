62deb3f2b7a9eaff82bbf55ecd770fd2d920962d
MengFin <mengfin@app.com>
2026-09-13 17:55:02 +0700

    fix: compileSdk 36 + ndkVersion 28.2.13676358

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 
 android {
     namespace = "com.example.mengfin"
-    compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    compileSdk = 36
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "com.example.mengfin"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 3
         versionName = "3.0.0"
     }
